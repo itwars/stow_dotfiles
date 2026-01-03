4a57e5333058388d73b83bc115b4dad28b0854f0
itwars <vincent.rabah@gmail.com>
2026-01-03 18:55:10 +0100

    add screencap

---
+++ hypr/.config/hypr/scripts/captureAll.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#set -euo pipefail
+
+SCREENSHOT_FILENAME=$(date +'%Y-%m-%dT%H:%M:%S%z_grim.jpg')
+SCREENSHOT_FILENAME_ABSOLUTE=$HOME/Images/$SCREENSHOT_FILENAME
+
+notify-send "Screen capture" "Capture to:" -i "$SCREENSHOT_FILENAME_ABSOLUTE"
+grim $SCREENSHOT_FILENAME_ABSOLUTE
