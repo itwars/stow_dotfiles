84a4cebe16546133be732560d2645ba942fffa21
itwars <vincent.rabah@gmail.com>
2026-01-04 17:59:41 +0100

    fix some stuffs

--- hypr/.config/hypr/scripts/captureAll.sh
+++ hypr/.config/hypr/scripts/captureAll.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#set -euo pipefail
+set -euo pipefail
 
 SCREENSHOT_FILENAME=$(date +'%Y-%m-%dT%H:%M:%S%z_grim.jpg')
 SCREENSHOT_FILENAME_ABSOLUTE=$HOME/Images/$SCREENSHOT_FILENAME
-
-notify-send "Screen capture" "Capture to:" -i "$SCREENSHOT_FILENAME_ABSOLUTE"
+notify-send "Screen capture" "Capture to: $SCREENSHOT_FILENAME_ABSOLUTE"
 grim $SCREENSHOT_FILENAME_ABSOLUTE
--- setup-mycomputer/setup-mycomputer.sh
+++ setup-mycomputer/setup-mycomputer.sh
@@ -1,5 +1,7 @@
 #!/bin/env sh
 sudo pacman -S waybar swaync hyprpaper hypridle wofi waybar mc neovim stow go tmux ttf-victor-mono-nerd
 sudo pacman -S blueman cpupower-gui nm-connection-editor
+sudo pacman -S slurp wl-clipboard grim
+sudo pacman -S ublock-origin onlyoffice-bin ungoogled-chromium-bin
 
 
