e462c2b0311bc0450c2b2ec4da8684c51cd1e352
itwars <vincent.rabah@gmail.com>
2026-01-15 16:41:34 +0100

    fix export path

--- bash/.bashrc.d/00exports.sh
+++ bash/.bashrc.d/00exports.sh
@@ -1,14 +1,19 @@
 export LC_NUMERIC="fr_FR.UTF-8"
-export GOPATH=$HOME/Documents/projects/lang/golang
+export COLORTERM=truecolor
 
 export PATH=$PATH:~/scripts:$HOME/ivra0940/gocode/bin/
 export PATH=$PATH:/sbin/
-export PATH=$PATH:$GOPATH/bin
 #export PATH=$PATH:$NPM_PACKAGES/bin
 export PATH=$PATH:~/.local/bin
-export PATH=$HOME/.cargo/bin:$PATH
 
-export COLORTERM=truecolor
 
 export _Z_OWNER=`whoami`
 
+
+# Golang setup 
+export GOPATH=$HOME/Documents/projects/lang/golang
+export GOROOT=$HOME/.local/lib/go_modules/
+export PATH=$PATH:$GOPATH/bin
+
+# Rust setup 
+export PATH=$HOME/.cargo/bin:$PATH
