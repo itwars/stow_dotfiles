bbefed69803b8cdeda75a506ed048249b2cf1042
itwars <vincent.rabah@gmail.com>
2026-02-10 17:36:19 +0100

    fix wallpapaers

--- hypr/.config/hypr/scripts/wallpaper.sh
+++ hypr/.config/hypr/scripts/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Configuration
 WALLPAPER_DIR="$HOME/.config/backgrounds/"  # Change this to your wallpaper directory
-CACHE_DIR="$HOME/.cache/wallpaper-selector"
+CACHE_DIR="$HOME/.cache/wallpaper-selector/"
 THUMBNAIL_WIDTH="250"  # Size of thumbnails in pixels (16:9)
 THUMBNAIL_HEIGHT="141"
 # Create cache directory if it doesn't exist
